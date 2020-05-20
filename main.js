e7af55126349ecf25e98c07bec62c09c7f72e00d
Veronica Celseti <veronicacelseti12@gmail.com>
2020-05-20 16:58:23 +0200

    FINITO SENZA BONUS

--- main.js
+++ main.js
@@ -5,14 +5,33 @@ $(document).ready(function(){
         'success': function(data) {
             var array_musica = data.response;
             console.log(array_musica);
-            for (var i = 0; i < array_musica.length; i++){
-                var elemento_corrente = array_musica[i];
-                console.log(elemento_corrente);
-            }
+            template(array_musica);
         },
         'error': function() {
             alert('Si è verificato un errore!');
         }
-
     });
 });
+function template(dischi) {
+    var template_html = $('#entry-template').html();
+    var template_function = Handlebars.compile(template_html);
+    var data = {
+        'poster': '' ,
+        'title':'' ,
+        'author': '',
+        'genre': '',
+        'year':''
+    }
+
+    for (var i = 0; i < dischi.length; i++){
+        var elemento_corrente = dischi[i];
+        console.log(elemento_corrente);
+        data.poster = elemento_corrente.poster;
+        data.title = elemento_corrente.title;
+        data.author = elemento_corrente.author;
+        data.genre = elemento_corrente.genre;
+        data.year = elemento_corrente.year;
+        var html = template_function(data);
+        $('.container').append(html);
+    }
+}
